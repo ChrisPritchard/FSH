d295d226f9cec08df77939b23f08b7764a98ec9a
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-02-04 09:14:42 +1300

    created a simple interface with some helper methods

---
+++ Program.fs
@@ -0,0 +1,29 @@
+﻿open Terminal
+
+[<EntryPoint>]
+let main _ =
+    colour "Green"
+
+    writeLine (sprintf "Welcome to - SYMENET -")
+
+    defaultColour ()
+
+    writeLine ""
+    writeLine ""
+    writeLine "If you don't know what to type next, try '?' or 'help'"
+
+    let prompt () = 
+        colour "Red"
+        write ".\> "
+        defaultColour ()
+        readLine ()
+
+    let rec endlessEcho () =
+        let entered = prompt ()
+        colour "yellow"
+        if entered <> "" then writeLine entered
+        endlessEcho ()
+
+    endlessEcho ()
+
+    0
---
+++ Terminal.fs
@@ -0,0 +1,20 @@
+﻿module Terminal
+
+open System
+
+let originalColour = ConsoleColor.Gray
+
+let defaultColour () = 
+    Console.ForegroundColor <- originalColour
+
+let colour s = 
+    let consoleColour = Enum.Parse (typeof<ConsoleColor>, s, true) :?> ConsoleColor
+    Console.ForegroundColor <- consoleColour
+
+let write (s : string) = Console.Write s
+
+let writeLine (s : string) = Console.WriteLine s
+
+let read () = Console.Read ()
+
+let readLine () = Console.ReadLine ()

2c933b02da5362a2299144acb8bad713fc54f04f
Christopher <chrispritchard.nz@gmail.com>
2019-02-07 12:09:31 +1300

    naieve impl of ls/dir

--- Builtins.fs
+++ Builtins.fs
@@ -1,10 +1,21 @@
 ﻿module Builtins
 
+open System.IO
+
 let echo args path = 
     printfn "%s" (String.concat " " args)
     path
 
+let dir args path = 
+    let searchPath = if List.length args = 0 then path else Path.Combine (path, args.[0])
+    let searchPattern = if List.length args <= 1 then "*" else args.[1]
+    Directory.GetDirectories (searchPath, searchPattern) |> Seq.iter (Path.GetFileName >> printfn "%s/")
+    Directory.GetFiles (searchPath, searchPattern) |> Seq.iter (Path.GetFileName >> printfn "%s")
+    path
+
 let builtins = 
     [
-        "echo", echo        
-    ] |> Map.ofList<string, seq<string> -> string -> string>
+        "echo", echo
+        "dir", dir
+        "ls", dir
+    ] |> Map.ofList<string, string list -> string -> string>

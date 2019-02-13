263254fb065feda5aa64f21dd437e894b4a75fc8
Chris Pritchard <chris.pritchard@edpay.nz>
2019-02-14 12:59:43 +1300

    Augmented parts to process parentheses too

--- Terminal.fs
+++ Terminal.fs
@@ -21,52 +21,31 @@ let colour s =
 /// The cursor should only be visible when accepting input from the user, and not when drawing the prompt, for example.
 let cursor b = Console.CursorVisible <- b
 
-/// Splits up a string into tokens, accounting for escaped spaces and quote wrapping,
+/// Splits up a string into tokens, accounting for escaped spaces and quote/code wrapping,
 /// e.g. '"hello" world "this is a" test\ test' would be processed as ["hello";"world";"this is a";"test test"].
 let parts s =
     // The internal recursive function processes the input one char at a time via a list computation expression. 
     // This affords a good deal of control over the output, and is functional/immutable.
-    // A slightly simpler way of doing this would be to use a loop with mutables; a commented out version of such an approach is below.
-    let rec parts soFar quoted last remainder = 
+    let rec parts soFar wrapped last remainder = 
         [
             if remainder = "" then yield soFar
             else
                 let c, next = remainder.[0], remainder.[1..]
-                match c with
-                | '\"' when soFar = "" -> 
-                    yield! parts soFar true last next
-                | '\"' when last <> '\\' && quoted ->
+                match c, wrapped with
+                | '(', None when soFar = "" ->
+                    yield! parts soFar (Some '(') last next
+                | ')', Some '(' when last <> '\\' ->
+                    yield sprintf "(%s)" soFar
+                    yield! parts "" None last next
+                | '\"', None when soFar = "" -> 
+                    yield! parts soFar (Some '\"') last next
+                | '\"', Some '\"' when last <> '\\' ->
                     yield sprintf "\"%s\"" soFar
-                    yield! parts "" false last next
-                | ' ' when last <> '\\' && not quoted ->
+                    yield! parts "" None last next
+                | ' ', None when last <> '\\' ->
                     if soFar <> "" then yield soFar
-                    yield! parts "" quoted last next
+                    yield! parts "" None last next
                 | _ -> 
-                    yield! parts (soFar + string c) quoted c next
+                    yield! parts (soFar + string c) wrapped c next
         ]
-    parts "" false ' ' s
-
-// Mutable version of parts above, done with a loop instead of recursion.
-(*
-let parts s = 
-    [
-        let mutable last = ' '
-        let mutable quoted = false
-        let mutable soFar = ""
-
-        for c in s do
-            match c with
-            | '\"' when soFar = "" -> quoted <- true
-            | '\"' when last <> '\\' && quoted ->
-                yield sprintf "\"%s\"" soFar
-                quoted <- false
-                soFar <- ""
-            | ' ' when last <> '\\' && not quoted ->
-                if soFar <> "" then yield soFar
-                soFar <- ""
-            | _ -> 
-                soFar <- soFar + string c
-                last <- c
-            
-        if soFar <> "" then yield soFar
-    ]*)
+    parts "" None ' ' s

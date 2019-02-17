3e372cf351afb588bd04dc8d5c80a0609fe24354
Christopher <chrispritchard.nz@gmail.com>
2019-02-17 15:14:01 +1300

    Added fsunit and created some parts tests

---
+++ test/Terminal.Parts.fs
@@ -0,0 +1,40 @@
+﻿module Terminal.Parts
+
+open FsUnit
+open FsUnit.Xunit
+open Xunit
+
+[<Fact>]
+let ``Parts parses blank as blank`` () =
+    let result = parts ""
+    result |> should be Empty
+
+[<Fact>]
+let ``Parts parses single as single`` () =
+    let result = parts "test"
+    result |> should equal ["test"]
+
+[<Fact>]
+let ``Parts parses two words`` () =
+    let result = parts "test1 test2"
+    result |> should equal ["test1";"test2"]
+
+[<Fact>]
+let ``Parts parses quotes properly`` () =
+    let result = parts "\"test1 test2\""
+    result |> should equal ["\"test1 test2\""]
+
+[<Fact>]
+let ``Parts parses brackets properly`` () =
+    let result = parts "(test1 test2)"
+    result |> should equal ["(test1 test2)"]
+
+[<Fact>]
+let ``Parts escapes quotes`` () =
+    let result = parts "\\\"test1 test2\\\""
+    result |> should equal ["\\\"test1";"test2\\\""]
+
+[<Fact>]
+let ``Parts escapes spaces`` () =
+    let result = parts "test1 te\ st2"
+    result |> should equal ["test1";"te\ st2"]

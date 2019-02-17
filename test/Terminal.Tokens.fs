283743b3c666063d1f46cea30b670bbbec377ff4
Christopher <chrispritchard.nz@gmail.com>
2019-02-17 15:19:25 +1300

    Added basic token tests

---
+++ test/Terminal.Tokens.fs
@@ -0,0 +1,26 @@
+﻿module Terminal.Tokens
+
+open FsUnit
+open FsUnit.Xunit
+open Xunit
+open Model
+
+[<Fact>]
+let ``Tokens parses blank as blank`` () =
+    let result = tokens []
+    result |> should be Empty
+
+[<Fact>]
+let ``Tokens parses no arg command`` () =
+    let result = tokens ["test"]
+    result |> should equal [Command ("test", [])]
+
+[<Fact>]
+let ``Tokens parses a command with args`` () =
+    let result = tokens ["test";"a1";"a2"]
+    result |> should equal [Command ("test", ["a1";"a2"])]
+
+[<Fact>]
+let ``Tokens parses code into a pipe into code`` () =
+    let result = tokens ["(10)";"|>";"(printfn \"%i\")"]
+    result |> should equal [Code "(10)";Pipe;Code "(printfn \"%i\")"]
